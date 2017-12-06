0a47551bed70a82b0cbc67412d508a5f37075eac
gplesz <gabor.plesz@gmail.com>
2017-12-06 15:06:42 +0100

    #3: api összekötése az IdentityServer (is4inmem)-mel

--- testapi/Startup.cs
+++ testapi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +24,28 @@ namespace testapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
+            //Nem kell a teljes MVC, elég az alap
+            services.AddMvcCore()
+                    //kell a válaszhoz a JSON formázás
+                    .AddJsonFormatters()
+                    //és a jogosultságkezelés is szükséges
+                    .AddAuthorization();
+
+            //És kell a felhasználóazonosítás,
+            //amit az Identity Server végez
+            services.AddAuthentication(
+                IdentityServerAuthenticationDefaults
+                    .AuthenticationScheme
+                ).AddIdentityServerAuthentication(
+                    o=>{
+                        //IDS címe
+                        o.Authority = "http://localhost:5000";
+                        //fejlesztés van, nem kell ssl
+                        o.RequireHttpsMetadata = false;
+                        o.ApiName = "api1";
+                        //o.ApiSecret = "secret";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +56,7 @@ namespace testapi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
